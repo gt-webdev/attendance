c78ee30c0229a44432dcfcc3e4bec30bb2ccd174
Ankit Shankar <ankitshankar@gmail.com>
2011-09-28 19:10:54 -0400

    More indentation fixes for generated files

--- app.js
+++ app.js
@@ -34,33 +34,33 @@ everyauth.helpExpress(app);
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({
-      secret: conf.session.secret,
-  }));
-  app.use(everyauth.middleware());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: conf.session.secret,
+    }));
+    app.use(everyauth.middleware());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+    app.use(express.errorHandler()); 
 });
 
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
 app.listen(3000);
