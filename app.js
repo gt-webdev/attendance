c8b1c453acbbcf4120932c726470a41f8e147e4a
Andrew Ash <ash211@gmail.com>
2011-09-27 23:34:58 -0700

    Create stubs for orgs URLs and handlers
    Several todos remaining, mostly remaining to Mongoose data interfacing

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: "mysupersecretkey"}));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -31,6 +33,40 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   res.render('index', {
     title: 'Express'
+  });
+});
+
+app.get('/orgs', function(req, res){
+  // TODO: actually read list of all orgs
+  var orgs = [{name: "Name1", desc: "Desc1"}, {name: "Name2", desc: "Desc2"}];
+
+  res.render('orgs', {
+    title: 'All Organizations',
+    orgs: orgs,
+  });
+});
+
+app.get('/orgs/:id', function(req, res){
+  // TODO: actually read org
+  var id = req.params.id;
+  var name = "This org's name";
+
+  res.render('orgs', {
+    title: 'Organization: ' + name,
+    flash: req.flash().info,
+  });
+});
+
+app.put('/orgs', function(req, res){
+  req.flash('info', 'Org created: ' + req.body.name);
+  // TODO: actually create org
+  var id = 1;
+  res.redirect('/orgs/' + id);
+});
+
+app.get('/create-org', function(req, res){
+  res.render('create-org', {
+    title: 'Create new org'
   });
 });
 
