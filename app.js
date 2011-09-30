cf76dd1dbc7aea1972ba39fa66723871f8367b36
Andrew Ash <ash211@gmail.com>
2011-09-29 23:49:21 -0700

    Auth cleanup
    Follow convention of having middleware calls be functions
     - improves extensibility in future if need to add parameters later
    Name exported functions
     - Better debuggability than anonymous functions in future stacktraces

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.configure(function(){
         secret: conf.session.secret,
     }));
     app.use(everyauth.middleware());
-    app.use(auth.middleware);
+    app.use(auth.middleware());
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
 });
