71925100fa05128266616a94a98ee14b7493ef49
Jarvis Johnson <magicjarvis@gmail.com>
2011-11-02 17:45:14 -0400

    Clean up for merge

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ This application will be used to keep track of attendance for Georgia Tech Colle
 ## Set Up
 
 Within the project, you're going to want to install all of the module dependencies in the project.
-	
+
     npm install
 
 You'll then want to run the install defaults script in order to configure the database
-	
+
     npm run-script install-defaults
--- app.js
+++ app.js
@@ -137,21 +137,21 @@ app.get('/events/:_id', function(req, res, next) {
             models.Event.findOne({_id: req.params._id}, cb);
         },
         function(event, cb) {
-        	models.Org.findOne({_id: event.org}, function(err, org) {
-        		cb(err, org, event);
-        	});
+            models.Org.findOne({_id: event.org}, function(err, org) {
+                cb(err, org, event);
+            });
         },
         function(event, org, cb) {
-        	models.Place.findOne({_id: event.place}, function(err, place) {
-        		cb(err, org, event, place);
-        	});
+            models.Place.findOne({_id: event.place}, function(err, place) {
+                cb(err, org, event, place);
+            });
         },
         function(event, org, place, cb) {
-        	async.map(event.attendees, function(user_id, cb) {
-        		models.User.findOne({_id: user_id}, cb);
-        	}, function(err, attendees) {
-				cb(err, event, org, place, attendees);
-        	});
+            async.map(event.attendees, function(user_id, cb) {
+                models.User.findOne({_id: user_id}, cb);
+            }, function(err, attendees) {
+                cb(err, event, org, place, attendees);
+            });
         },
     ], function(err, event, org, place, attendees) {
         if (err) {
@@ -178,25 +178,25 @@ app.get('/events', function(req, res, next) {
             models.Event.find({}, cb);
         },
         function(events, cb) {
-        	async.map(events, function(event, cb) {
-        		models.Org.findOne({_id: event.org}, cb);
-        	}, function(err, orgs) {
-				cb(err, events, orgs);
-        	});
+            async.map(events, function(event, cb) {
+                models.Org.findOne({_id: event.org}, cb);
+            }, function(err, orgs) {
+                cb(err, events, orgs);
+            });
         },
         function(events, orgs, cb) {
-        	cb(null, events.map(function(x,i) {
-    			return {
-    				event: x,
-    				org: orgs[i],
-    			};
-    		}));
+            cb(null, events.map(function(x,i) {
+                return {
+                    event: x,
+                    org: orgs[i],
+                };
+            }));
         },
     ], function(err, events) {
         if (err) {
             return next(err);
         }
-        
+
         res.render('events', {
             title: 'Events',
             events: events,
