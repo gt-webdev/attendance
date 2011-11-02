c93984a6e45b737078300356e8dbe9fe4a184450
Jarvis Johnson <magicjarvis@gmail.com>
2011-11-02 17:51:08 -0400

    Undo port change

--- app.js
+++ app.js
@@ -204,5 +204,5 @@ app.get('/events', function(req, res, next) {
     });
 });
 
-app.listen(4000);
+app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
