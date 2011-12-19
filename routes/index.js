981dff3ba1527253ac8aeaa0b30a209624f1642a
Andrew Ash <ash211@gmail.com>
2011-12-19 00:56:29 -0800

    Extract route registering out to routes/

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,24 @@
 exports.events = require('./events');
 exports.orgs = require('./orgs');
+
+var orgs   = exports.orgs;
+var events = exports.events;
+var auth   = require('../lib/auth');
+
+exports.registerOn = function(app) {
+    app.get('/', function(req, res) {
+        res.redirect('/orgs');
+    });
+
+    app.get('/orgs', orgs.list);
+    app.get('/orgs/:slug', orgs.details);
+    app.get('/create-org', auth.loginRequired, orgs.create);
+    app.post('/orgs', auth.loginRequired, orgs.post);
+    app.delete('/orgs/:slug', auth.loginRequired, orgs.delete);
+
+    app.get('/events', events.list);
+    app.get('/events/:id', events.details);
+    app.get('/create-event', auth.loginRequired, events.create);
+    app.post('/events', auth.loginRequired, events.post);
+    app.delete('/events/:id', auth.loginRequired, events.delete);
+};
