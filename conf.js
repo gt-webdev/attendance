2aea6f1e14ddc18bacf156fe492623e8288ff162
Ankit Shankar <ankitshankar@gmail.com>
2012-01-01 17:49:05 -0500

    Make things Heroku friendly

--- conf.js
+++ conf.js
@@ -1,1 +1,8 @@
-module.exports = require('./conf.local');
+module.exports = {
+    mongo: {
+        uri: process.env.MONGOLAB_URI || 'mongodb://localhost/attendance',
+    },
+    session: {
+        secret: process.env.SESSION_SECRET || 'heheheh secret',
+    },
+};
