3406d243d4870458553de432eee6bb28f92218e7
Yuval Dekel <thedekel@gmail.com>
2012-05-10 19:26:35 -0400

    implemented kiosk mode
    any event's page + '/kiosk' will show kiosk mode
    guests can sign in
      if the user is found in the database, his user is added
      else, if the guest account exists, the guest is added
      else, a guest is created and added to the event's guest list
    mods and admins may remove guests from events from the details page
    mods and admin may see guests' gtids on the details page
    when a user registers, if the email and gtid appear on a guest,
      that guest is removed and his "participations" are added to the
      new user
    added links on the '/events/' page for kiosk mode
    added a link on details page for kiosk mode
    details and kiosk page now display two seperate guest-lists:
      one for registered guests (showing full names) and
      one for guetss, showing emails

--- routes/events.js
+++ routes/events.js
@@ -62,8 +62,6 @@ exports.delete = function(req, res, next) {
                 }
                 if (req.user.is_admin ||
                         (org && org.admins.indexOf(req.user.id) != -1)) {
-                    //delete participation elements before deleting event
-                    models.Part.remove({'event':event._id});
                     return cb(null, event);
                 }
                 res.send(403);
@@ -134,19 +132,53 @@ exports.details = function(req, res, next) {
                 cb(err, event, org, place, attendees);
             });
         },
-        function(event, org, place, legacy_attendees){
+        function(event, org, place, legacy_attendees, cb){
             models.Part.find({'event':event._id}, ['account'],
                 function(err, docs){
                     async.map(docs, function(user_id, cb) {
-                        models.User.findOne({_id: user_id}, cb);
+                        models.User.findOne({_id: user_id.account}, function(err,att){
+                            cb(null, att);
+                        });
                     }, function(err, new_attendees) {
-                        cb(err, event, org, place, legacy_attendees, new_attendees);
+                      async.filter(new_attendees, function(att, fcb){
+                        if (att){
+                          return fcb(true);
+                        }else{
+                          return fcb(false);
+                        }
+                      },function(user_att){
+                        cb(err, event, org, place, legacy_attendees, user_att);
+                      });
                     });
-
+                }
+            );
+        },
+        function(event,org,place,legacy_attendees,attendees, cb){
+          models.Part.find({'event':event._id}, ['account'],
+                function(err, docs){
+                    async.map(docs, function(user_id, cb) {
+                        models.Guest.findOne({_id: user_id.account}, function(err,att){
+                            cb(null, att);
+                        });
+                    }, function(err, new_attendees) {
+                      async.filter(new_attendees, function(att, fcb){
+                        if (att){
+                          return fcb(true);
+                        }else{
+                          return fcb(false);
+                        }
+                      },function(guest_att){
+                          async.map(docs, function(doc,cb){
+                              cb(null, doc.account);
+                          }, function(err, final_att){
+                              cb(err, event, org, place, legacy_attendees, attendees, guest_att, final_att);
+                          });
+                      });
+                    });
                 }
             );
         }
-    ], function(err, event, org, place, legacy_attendees, attendees ) {
+    ], function(err, event, org, place, legacy_attendees, attendees, guests, att_ids) {
         if (err) {
             return next(err);
         }
@@ -157,78 +189,14 @@ exports.details = function(req, res, next) {
             event: event,
             org: org,
             place: place,
-            attendees: attendees,
+            att_users: attendees,
+            att_guests: guests,
+            atts: att_ids,
             legacy:legacy_attendees,
         });
     });
 };
 
-exports.kiosk = function(req,res,next) {
-  async.waterfall([
-        function(cb) {
-            models.Event.findOne({_id: req.params.id}, function(err, event) {
-                if (event == null) {
-                    return res.send(404);
-                } else {
-                    cb(err, event);
-                }
-            });
-        },
-        function(event, cb) {
-            models.Org.findOne({_id: event.org}, function(err, org) {
-                if (org == null) {
-                    res.send(404);
-                } else {
-                    cb(err, event, org);
-                }
-            });
-        },
-        function(event, org, cb) {
-            models.Place.findOne({_id: event.place}, function(err, place) {
-                cb(err, event, org, place);
-            });
-        },
-        function(event, org, place, cb) {
-            /*this is a map which looks for user based on the
-             * attendees list which exist within the "event" object
-             * We now use a combination of a participation and user/guest
-             * models to accomplish this. However, this is kept for legacy
-             * support purposes - NOTE: if the database is ever restarted,
-             * this functionality should be safe to remove! */
-            async.map(event.attendees, function(user_id, cb) {
-                models.User.findOne({_id: user_id}, cb);
-            }, function(err, attendees) {
-                cb(err, event, org, place, attendees);
-            });
-        },
-        function(event, org, place, legacy_attendees){
-            models.Part.find({'event':event._id}, ['account'],
-                function(err, docs){
-                    async.map(docs, function(user_id, cb) {
-                        models.User.findOne({_id: user_id}, cb);
-                    }, function(err, new_attendees) {
-                        cb(err, event, org, place, legacy_attendees, new_attendees);
-                    });
-
-                }
-            );
-        }
-    ], function(err, event, org, place, legacy_attendees, attendees ) {
-        if (err) {
-            return next(err);
-        }
-        if (!event) {
-            return res.send(404);
-        }
-        res.render('event', {
-            event: event,
-            org: org,
-            place: place,
-            attendees: attendees,
-            legacy: legacy_attendees,
-        });    
-    });
-}
 
 exports.list = function(req, res, next) {
     async.waterfall([
@@ -274,34 +242,197 @@ exports.list = function(req, res, next) {
     });
 };
 
-exports.put = function(req, res, next) {
-    if (!req.body._type) {
-        res.send(400); // Bad Request
-    }
-    if (req.body._type === 'attend') {
-        exports.attend(req, res, next);
-    } else if (req.body._type === 'unattend') {
-        exports.unattend(req, res, next);
-    } else if (req.body._type === 'update') {
-        exports.update(req, res, next);
-    }
+exports.kiosk = function(req,res,next) {
+  async.waterfall([
+        function(cb) {
+            models.Event.findOne({_id: req.params.id}, function(err, event) {
+                if (event == null) {
+                    return res.send(404);
+                } else {
+                    cb(err, event);
+                }
+            });
+        },
+        function(event, cb) {
+            models.Org.findOne({_id: event.org}, function(err, org) {
+                if (org == null) {
+                    res.send(404);
+                } else {
+                    cb(err, event, org);
+                }
+            });
+        },
+        function(event, org, cb) {
+            models.Place.findOne({_id: event.place}, function(err, place) {
+                cb(err, event, org, place);
+            });
+        },
+        function(event, org, place, cb) {
+            /*this is a map which looks for user based on the
+             * attendees list which exist within the "event" object
+             * We now use a combination of a participation and user/guest
+             * models to accomplish this. However, this is kept for legacy
+             * support purposes - NOTE: if the database is ever restarted,
+             * this functionality should be safe to remove! */
+            async.map(event.attendees, function(user_id, cb) {
+                models.User.findOne({_id: user_id}, cb);
+            }, function(err, attendees) {
+                cb(err, event, org, place, attendees);
+            });
+        },
+        function(event, org, place, legacy_attendees, cb){
+            models.Part.find({'event':event._id}, ['account'],
+                function(err, docs){
+                    async.map(docs, function(user_id, cb) {
+                        models.User.findOne({_id: user_id.account}, function(err,att){
+                            cb(null, att);
+                        });
+                    }, function(err, new_attendees) {
+                      async.filter(new_attendees, function(att, fcb){
+                        if (att){
+                          return fcb(true);
+                        }else{
+                          return fcb(false);
+                        }
+                      },function(user_att){
+                        cb(err, event, org, place, legacy_attendees, user_att);
+                      });
+                    });
+                }
+            );
+        },
+        function(event,org,place,legacy_attendees,attendees, cb){
+          models.Part.find({'event':event._id}, ['account'],
+                function(err, docs){
+                    async.map(docs, function(user_id, cb) {
+                        models.Guest.findOne({_id: user_id.account}, function(err,att){
+                            cb(null, att);
+                        });
+                    }, function(err, new_attendees) {
+                      async.filter(new_attendees, function(att, fcb){
+                        if (att){
+                          return fcb(true);
+                        }else{
+                          return fcb(false);
+                        }
+                      },function(guest_att){
+                          async.map(docs, function(doc,cb){
+                              cb(null, doc.account);
+                          }, function(err, final_att){
+                              cb(err, event, org, place, legacy_attendees, attendees, guest_att, final_att);
+                          });
+                      });
+                    });
+                }
+            );
+        }
+    ], function(err, event, org, place, legacy_attendees, attendees, guests, att_ids) {
+        if (err) {
+            return next(err);
+        }
+        if (!event) {
+            return res.send(404);
+        }
+        res.render('kiosk', {
+            event: event,
+            org: org,
+            place: place,
+            att_users: attendees,
+            att_guests: guests,
+            atts: att_ids,
+            legacy:legacy_attendees,
+        });
+    });
 };
 
 /*
- * guest function is used in kiosk mode to register "guests" instead 
- * of users. if the guest information corresponds to a user, that 
- * user is registered instead. */
+        function(event, org, place, legacy_attendees, cb){
+          debugger;
+            models.Part.find({'event':event._id}, ['account'],
+                function(err, docs){
+                    async.map(docs, function(user_id, cb) {
+                        models.User.findOne({_id: user_id.account}, function(err,att){
+                            if(att){
+                                cb(null, att);
+                            }else{
+                                models.Guest.findOne({_id:  user_id.account});
+                            }
+                        });
+                    }, function(err, new_attendees) {
+                        cb(err, event, org, place, legacy_attendees, new_attendees);
+                    });
+
+                }
+            );
+        }
+    ], function(err, event, org, place, legacy_attendees, attendees ) {
+        if (err) {
+            return next(err);
+        }
+        if (!event) {
+            return res.send(404);
+        }
+        console.log(attendees);
+        res.render('kiosk', {
+            event: event,
+            org: org,
+            place: place,
+            attendees: attendees,
+            legacy: legacy_attendees,
+        });    
+    });
+}
+*/
 exports.guest = function(req, res, next) {
+    if (!req.body._type) {
+        res.send(400); // Bad Request
+    }
+    if (req.body._type === 'guest') {
+        exports.guest_attend(req, res, next);
+    }
+};
+
+exports.guest_attend = function(req, res, next) {
     async.waterfall([
         function(cb) {
             models.Event.findOne({_id: req.params.id}, cb);
         },
         function(event, cb) {
-            if (event.attendees.indexOf(req.user.id) < 0
-                    && (!event.passkey || event.passkey === req.body.passkey)) {
-                event.attendees.push(req.user.id);
-                event.save();
-            }
+            models.User.findOne({'email':req.body.email, 'gt_id':req.body.gtid},
+                function(err,doc){
+                    debugger;
+                    if (doc==null){
+                        models.Guest.findOne({'email':req.body.email, 'gt_id':req.body.gtid},
+                            function(err,guest_doc){
+                                var my_doc;
+                                if (!guest_doc){
+                                    my_doc = new models.Guest({
+                                        'email':req.body.email,
+                                        'gt_id':req.body.gtid
+                                    });
+                                    my_doc.save();
+                                } else {
+                                    my_doc = guest_doc;
+                                }
+                                cb(null,event, my_doc);
+                        });
+                    }else{
+                        cb(null,event,doc)
+                    }
+                });
+        },
+        function(event, user, cb) {
+            models.Part.count({'event':event._id, 'account':user._id},
+                    function(err, count){
+                        if (count==0){
+                            var new_part = new models.Part({
+                                    'event':event._id,
+                                    'account':user._id,
+                                    'date':new Date()
+                            });
+                            new_part.save()
+                        }
+            });
             cb();
         },
     ], function(err) {
@@ -312,17 +443,57 @@ exports.guest = function(req, res, next) {
     });
 };
 
+exports.put = function(req, res, next) {
+    if (!req.body._type) {
+        res.send(400); // Bad Request
+    }
+    if (req.body._type === 'attend') {
+        exports.attend(req, res, next);
+    } else if (req.body._type === 'unattend') {
+        exports.unattend(req, res, next);
+    } else if (req.body._type === 'update') {
+        exports.update(req, res, next);
+    } else if (req.body._type === 'unguest'){
+        exports.unguest(req, res, next);
+    }
+};
+
+exports.unguest = function(req,res,next){
+    async.waterfall([
+        function(cb) {
+            models.Event.findOne({_id: req.params.id}, cb);
+        },
+        function(event, cb) {
+            models.Part.remove({'event':event._id, 'account':req.body.guest},
+                function(err){
+                    cb();
+            });
+        },
+    ], function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect(req.url);
+    });
+}
+
 exports.attend = function(req, res, next) {
     async.waterfall([
         function(cb) {
             models.Event.findOne({_id: req.params.id}, cb);
         },
         function(event, cb) {
-            if (event.attendees.indexOf(req.user.id) < 0
-                    && (!event.passkey || event.passkey === req.body.passkey)) {
-                event.attendees.push(req.user.id);
-                event.save();
-            }
+            models.Part.count({'event':event._id, 'account':req.user.id},
+                function(err,count){
+                    if (count==0){
+                        var new_part = new models.Part({
+                                'event':event._id,
+                                'account':req.user.id,
+                                'date':new Date()
+                        });
+                        new_part.save()
+                    }
+            });
             cb();
         },
     ], function(err) {
@@ -339,13 +510,10 @@ exports.unattend = function(req, res, next) {
             models.Event.findOne({_id: req.params.id}, cb);
         },
         function(event, cb) {
-            var index = event.attendees.indexOf(req.user.id);
-            if (index < 0) {
-                return res.send(404);
-            }
-            event.attendees.splice(index, 1);
-            event.save();
-            cb();
+            models.Part.remove({'event':event._id, 'account':req.user.id},
+                function(err){
+                    cb();
+            });
         },
     ], function(err) {
         if (err) {
