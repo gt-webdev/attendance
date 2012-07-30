8fd1d33edf289f20bc738422272cc6eb9e58664e
Dekel <thedekel@gmail.com>
2012-07-30 19:09:41 -0400

    incorrect port used for testing

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 6696,
+    port: process.env.PORT || 3000,
     mongo: {
         uri: process.env.MONGOLAB_URI || 'mongodb://localhost/attendance',
     },
