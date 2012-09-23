fcdb73af2e5ec6e5bfab2beb1128a644ec40eb6c
Yuval Dekel <thedekel@gmail.com>
2012-09-23 15:49:23 -0400

    added js to display all users on /admin/users

--- public/javascripts/userman.js
+++ public/javascripts/userman.js
@@ -1,10 +1,45 @@
 (function(){
   var i,
       newdiv,
-      ulistdiv = document.getElementById("userlist");
+      ulistdiv = document.getElementById("userlist"),
+      table = document.createElement("table"),
+      createTh = function(header, cb){
+        var newth = document.createElement('th');
+        newth.innerHTML = header;
+        if (cb){
+          cb(newth);
+          return;
+        }
+        return newth;
+      },
+      createTr = function(user, cb){
+        var newtr = document.createElement("tr"),
+            emailtd, nametd, gtidtd, optd;
+        emailtd = document.createElement("td");
+        nametd = document.createElement("td");
+        gtidtd = document.createElement("td");
+        optd = document.createElement("td");
+        emailtd.innerHTML = user.email;
+        nametd.innerHTML = user.name.first + ' ' + user.name.last;
+        gtidtd.innerHTML = user.gt_id;
+        optd.innerHTML = "COMMANDS";
+        newtr.appendChild(emailtd);
+        newtr.appendChild(nametd);
+        newtr.appendChild(gtidtd);
+        newtr.appendChild(optd);
+        if (cb){
+          return cb(newtr, user);
+        }
+        return newtr;
+      }, usertr;
+  ["email", "name", "gtid", "operations"].forEach(function(header){
+    var hth = createTh(header);
+    table.appendChild(hth);
+  });
+  ulistdiv.appendChild(table);
   for (i = 0; i < users.length; i += 1){
-    newdiv = document.createElement("div");
-    newdiv.innerHTML = JSON.stringify(users[i]);
-    ulistdiv.appendChild(newdiv);
+    usertr = createTr(users[i]);
+    table.appendChild(usertr);
   }
+  document.getElementsByClassName("content")[0].appendChild(table);
 }());
